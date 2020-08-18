dcd7dbf86f1dabcadf5a5f0e86c7d4593ff155d2
adianer <962622577@qq.com>
2020-08-18 12:27:03 +0800

    1.0 项目搭建

---
+++ src/dao/CommentDao .java
@@ -0,0 +1,28 @@
+package dao;
+
+import entity.Comment;
+
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CommentDao {
+    //获取显示列表
+    public List getshowlist(Page page) throws Exception;
+
+
+    //获取总记录数
+    public int getCount();
+
+    //添加评论
+    public int insertComment(List<Object> objects );
+
+    //根据ID删除
+    public int deleteComment(int id);
+
+    //根据id查询
+    public Video queryCommentById(int id);
+
+    //修改评论
+    public int updataComment(List<Object> objects );
+}
